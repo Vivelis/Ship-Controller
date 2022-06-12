c50035a464260be5ca35bc416cf9f87c5a0d95cf
Le-Scientivic <107316919+Le-Scientivic@users.noreply.github.com>
2022-06-12 14:50:37 +0200

    Update programme_test.ino

--- Programme_arduino/programme_test/programme_test.ino
+++ Programme_arduino/programme_test/programme_test.ino
@@ -1,9 +1,28 @@
-void setup() {
-  // put your setup code here, to run once:
+#include <SoftwareSerial.h>
+#include <SerialCommand.h>
+SerialCommand sCmd;
 
+void setup() {
+    Serial.begin(9600);
+    while (!Serial);
+
+    sCmd.addCommand("PING", pingHandler);
 }
 
-void loop() {
-  // put your main code here, to run repeatedly:
+void loop () {
+    if (Serial.available() > 0)
+        sCmd.readSerial();
+}
 
+void pingHandler (const char *command) {
+    Serial.println("PONG");
+}
+
+void echoHandler () {
+  char *arg;
+  arg = sCmd.next();
+  if (arg != NULL)
+    Serial.println(arg);
+  else
+    Serial.println("nothing to echo");
 }
